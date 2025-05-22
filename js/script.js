2b4eef3a0fb28f3791110dbdd3d271766dff2458
DIABLO4CODE <fail.bariv@gmail.com>
2025-05-22 18:14:47 +0300

    3.6 финиш

--- js/script.js
+++ js/script.js
@@ -6,21 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const headerButtonEnter = document.querySelector('.header__button-enter');
     const modalApplication = document.querySelector(".applications");
     if (headerButtonEnter && modalApplication) {
-    headerButtonEnter.addEventListener("click",() => {
-        console.log('нажата кнопка войти');
-        modalApplication.removeAttribute("hidden");
-        const closeModalButton = document.querySelector(".application__close");
-        closeModalButton.addEventListener("click", () => {
-            modalApplication.setAttribute("hidden", true);
-            
-        });        
-     });
+        headerButtonEnter.addEventListener("click", () => {
+            console.log('нажата кнопка войти');
+            modalApplication.removeAttribute("hidden");
+            const closeModalButton = document.querySelector(".application__close");
+            closeModalButton.addEventListener("click", () => {
+                modalApplication.setAttribute("hidden", true);
+
+            });
+        });
     }
     window.addEventListener("click", (event) => {
         if (event.target === modalApplication) {
             modalApplication.setAttribute("hidden", true);
         }
-    });  
+    });
     //3.4 появление описания
     const factsImg = document.querySelectorAll('.facts__img');
     factsImg.forEach((item, index) => {
@@ -30,29 +30,29 @@ document.addEventListener("DOMContentLoaded", () => {
             item.style.opacity = 0.5;
             factsText[index].removeAttribute('hidden');
         });
-            item.addEventListener('mouseleave', () => {
-                item.style.opacity = 1;
-                factsText[index].setAttribute('hidden', true);
-            });
-            });
-            //3.4 появление имен преподавателей
-            const teachersContainer = document.querySelector(".teachers_container");
-            if (teachersContainer) {
-                const dataTitleTeachers = [
-                    "Мулярчик Наталья Николаевна",
-                    "Николаев Роман Игоревич",
-                    "Бариев Фаиль Нурфисович",
-                ];
-                const titleTeachers =
-                teachersContainer.querySelectorAll(".teachers__name");
-                titleTeachers.forEach((item, index) => {
-                    item.textContent = dataTitleTeachers [index];
-           });
+        item.addEventListener('mouseleave', () => {
+            item.style.opacity = 1;
+            factsText[index].setAttribute('hidden', true);
+        });
+    });
+    //3.4 появление имен преподавателей
+    const teachersContainer = document.querySelector(".teachers_container");
+    if (teachersContainer) {
+        const dataTitleTeachers = [
+            "Мулярчик Наталья Николаевна",
+            "Николаев Роман Игоревич",
+            "Бариев Фаиль Нурфисович",
+        ];
+        const titleTeachers =
+            teachersContainer.querySelectorAll(".teachers__name");
+        titleTeachers.forEach((item, index) => {
+            item.textContent = dataTitleTeachers[index];
+        });
     }
 });
 //3.5 навигацонное меню
 const headerMenu = document.querySelector('.header_menu');
-if (headerMenu){
+if (headerMenu) {
     const headerList = headerMenu.querySelector('.menu');
     const menuData = {
         link1: {
@@ -76,7 +76,7 @@ if (headerMenu){
             title: 'Пробное занятие',
         }
     }
-    const createLink = (UrlLink, title) =>{
+    const createLink = (UrlLink, title) => {
         const link = `
         <li class="menu_item">
         <a href="${UrlLink}" class="menu_link">${title}</a></li>
@@ -98,18 +98,51 @@ if (cardsCon) {
     const apiUrl = "data.json";
     const createCard = (
         imageUrl,
-            iconAlt,
-            iconWidth,
-            iconHeight,
-            title,
-            description
+        iconAlt,
+        iconWidth,
+        iconHeight,
+        title,
+        description
     ) => {
         const card = `
-        <li class="" href="#teachers">
-                      <img class="" src="${imageUrl}" alt="${iconAlt}" width="${iconWidth}" height="${iconHeight}">
-                    <h3 class="">${title}</h3>
-                    <p class="">${description}</p>
-                </li>`
+                <div class="teachers_card">
+                <img class="teachers__img" src="${imageUrl}" alt="${iconAlt}" width="${iconWidth}" height="${iconHeight}">
+                <p class="teachers__name">${title}</p>
+                <p class="teachers__job">${description}</p>
+            </div>
+            `;
+        return card;
+    };
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);//данные 
+            console.log(typeof data); // Тип полученных 
 
-    }
+            data.forEach((item) => {
+                const cardElement = createCard(
+                    item.image,
+                    item.iconAlt,
+                    item.iconWidth,
+                    item.iconHeight,
+                    item.title,
+                    item.description
+                );
+                cardList.insertAdjacentHTML("beforeend", cardElement);
+            });
+        })
+        .catch((error) => {
+            console.error("Ошибка при загрузке данных:", error);
+        });
+}
+//3.6 задание 2
+const preloader = document.querySelector(".preloader");
+const content = document.querySelector(".content");
+if (preloader && content) {
+    setTimeout(() => {
+        preloader.style.opacity ="0";
+        preloader.style.visibility ="hidden";
+        content.style.display ="block";
+        preloader.remove();
+    }, 3000);
 }
